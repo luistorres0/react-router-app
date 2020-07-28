923eca0c9d4a1b17a333aebe70fd812fe72bb9c7
Luis Torres <luisdtor@gmail.com>
2020-07-27 19:30:47 -0500

    Updates

---
+++ precache-manifest.7728d6ba97785af2b6403fdeadd5ed48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b842aac427b61bf2a164a63827fffd64",
+    "url": "/luistorres0/todoList.git/index.html"
+  },
+  {
+    "revision": "4fee0fdc24872465c72e",
+    "url": "/luistorres0/todoList.git/static/css/main.7bd8574c.chunk.css"
+  },
+  {
+    "revision": "291713a3333b914007d5",
+    "url": "/luistorres0/todoList.git/static/js/2.2ac06bc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/luistorres0/todoList.git/static/js/2.2ac06bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fee0fdc24872465c72e",
+    "url": "/luistorres0/todoList.git/static/js/main.ef6dad34.chunk.js"
+  },
+  {
+    "revision": "f3df250d7560143a4746",
+    "url": "/luistorres0/todoList.git/static/js/runtime-main.6ca898fc.js"
+  }
+]);
